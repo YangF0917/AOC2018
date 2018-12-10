2dd649d7a3c507fecfbb105f9d3993986bf0a760
Felix <felix.yang19@ycdsbk12.ca>
2018-12-10 17:25:55 -0500

    Add scanner to d11 files

--- advent2018/src/advent2018/day11q1.java
+++ advent2018/src/advent2018/day11q1.java
@@ -1,10 +1,11 @@
 package advent2018;
-
+import java.util.*;
 public class day11q1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
