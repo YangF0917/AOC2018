6369ca657663c98e44f418eb3557c85f10b1fa3c
Felix <felix.yang19@ycdsbk12.ca>
2018-12-11 21:42:08 -0500

    Complete d11q2

--- advent2018/src/advent2018/day11q1.java
+++ advent2018/src/advent2018/day11q1.java
@@ -5,7 +5,30 @@ public class day11q1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+		int gId = sc.nextInt();
+		int [][]fuel = new int [301][301];
+		for (int i = 1; i < 301; i++) {
+			for (int j = 1; j < 301; j++) {
+				fuel[i][j] = ((i + 10) * j + gId)*(i+10);
+				fuel[i][j] %= 1000; fuel[i][j] /= 100 - 5;
+			}
+		}
+		int maxSum = Integer.MIN_VALUE;
+		String ans = "0,0";
+		for (int i = 1; i < 301; i++) {
+			for (int j = 1; j < 301; j++) {
+				int sum = 0;
+				int min = 300-i;
+				for(int k = 0; k < 3; k++) {
+					for (int l = 0; l < 3; l++) {
+						sum += fuel[i+k][j+l];
+					}
+				}
+				if (sum > maxSum) {
+					maxSum = sum; ans = i + "," + j;
+				}
+			}
+		}
+		System.out.println(ans);
 	}
-
 }
--- advent2018/src/advent2018/day11q2.java
+++ advent2018/src/advent2018/day11q2.java
@@ -5,6 +5,36 @@ public class day11q2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		int gId = 8868;
+		int [][]fuel = new int [301][301];
+		for (int i = 1; i < 301; i++) {
+			for (int j = 1; j < 301; j++) {
+				fuel[i][j] = ((i + 10) * j + gId)*(i+10);
+				fuel[i][j] %= 1000; fuel[i][j] /= 100; fuel[i][j]-=5;
+			}
+		}
+		int maxSum = Integer.MIN_VALUE;
+		String ans = "0,0,0";
+		for (int i = 1; i < 301; i++) {
+			for (int j = 1; j < 301; j++) {
+				int min = 300-i;
+				int min2 = 300-j;
+				if (min2 < min) {
+					min = min2;
+				}
+				for (int m = 0; m <= min; m++) {
+				int sum = 0;
+				for(int k = 0; k <= m; k++) {
+					for (int l = 0; l <= m; l++) {
+						sum += fuel[i+k][j+l];
+					}
+				}
+				if (sum > maxSum) {
+					maxSum = sum; ans = i + "," + j + "," + m;
+				}
+				}
+			}
+		}
+		System.out.println(ans);
 	}
-
 }
