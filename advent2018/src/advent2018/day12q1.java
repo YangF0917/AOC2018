dd66f276fd8840b582beee0d0af9c62c9a56f491
Felix <felix.yang19@ycdsbk12.ca>
2018-12-12 09:27:31 -0500

    Complete d12q1

---
+++ advent2018/src/advent2018/day12q1.java
@@ -0,0 +1,50 @@
+package advent2018;
+import java.util.*;
+public class day12q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		// - 7, Starts at -7
+		// 2268 too low
+		// 3478 too high
+		String init = ".......#........#.#.#...###..###..###.#..#....###.###.#.#...####..##..##.#####..##...#.#.....#...###.#.####............................................................";
+		String myString = "..";
+		LinkedList<String> empty = new LinkedList<String>();
+		LinkedList<String> good = new LinkedList<String>();
+		String input = sc.nextLine();
+		while (!input.equals("0")){
+			if (input.charAt(input.length()-1) == '#') {
+				good.add(input.substring(0, 5));
+			}
+			else {
+				empty.add(input.substring(0, 5));
+			}
+			input = sc.nextLine();
+		}
+		System.out.println(empty);
+		System.out.println(good);
+		for (int i = 0; i < 20; i++) {
+			for (int j = 2; j < init.length()-2; j++) {
+				String temp = init.substring(j-2, j+3);
+				if (empty.indexOf(temp) != -1) {
+					myString += ".";
+				}
+				else {
+					myString += "#";
+				}
+			}
+			init = myString + "..";
+			myString = "..";
+		}
+		System.out.println(init);
+		int sum = 0;
+		for (int i = 0; i < init.length(); i++) {
+			if (init.charAt(i) == '#') {
+				sum += (i - 7);
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
