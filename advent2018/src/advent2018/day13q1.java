0aa7390ad0d36a9ced1e7ae08fbcc5246e89e293
Felix <felix.yang19@ycdsbk12.ca>
2018-12-12 22:00:42 -0500

    Create d13q1-2 files

---
+++ advent2018/src/advent2018/day13q1.java
@@ -0,0 +1,10 @@
+package advent2018;
+
+public class day13q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ advent2018/src/advent2018/day13q2.java
@@ -0,0 +1,10 @@
+package advent2018;
+
+public class day13q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
